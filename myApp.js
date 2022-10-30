4dab5de1be49d6f567d1fe383929e8cb7119746e
Alessio Morteo <alessiomorteo@users.noreply.github.com>
2022-10-30 20:06:44 +0000

    Serve html file using res.sendFile

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
 let express = require('express');
 let app = express();
 
+/*
 app.get('/', (req, res) => {
     res.send('Hello Express');
 })
+*/
 // see this using port 3000 at http://localhost:3000
+
+app.get('/', (req, res) => {
+    let absolutePath = __dirname + '/views/index.html';
+    res.sendFile(absolutePath);
+}) // serve the index.html file using res.sendFile method
+
 
 
 
