9749bcbec56440d00e0dd6db616315c91e195f1f
Alessio Morteo <alessiomorteo@users.noreply.github.com>
2022-10-30 19:57:33 +0000

    Added get function to dummy server

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello World")
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+})
+// see this using port 3000 at http://localhost:3000
 
 
 
